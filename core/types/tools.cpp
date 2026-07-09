5353329a900bf580886c2a16edfe5b9d85aa204b
petukhow <astronn@gmail.com>
2026-07-09 17:55:18 +0300

    fix wrong json parsing for google tool format

--- core/types/tools.cpp
+++ core/types/tools.cpp
@@ -7,11 +7,10 @@
 void to_json(nlohmann::json& j, const ToolInfo& t) {
     if (t.thought_signature.has_value()) {
     j = nlohmann::json{{"id", t.id}, {"name", t.name},
-    {"arguments", t.arguments}, {"thought_signature", t.thought_signature}};
-    }
-    else {
-         j = nlohmann::json{{"id", t.id}, {"name", t.name},
-    {"arguments", t.arguments}};
+        {"arguments", t.arguments}, {"thought_signature", t.thought_signature}};
+    } else {
+        j = nlohmann::json{{"id", t.id}, {"name", t.name},
+        {"arguments", t.arguments}};
     }
 }
 
@@ -20,7 +19,7 @@ void from_json(const nlohmann::json& j, ToolInfo& t) {
     j.at("name").get_to(t.name);
     j.at("arguments").get_to(t.arguments);
     if (j.contains("thought_signature")) {
-        t.thought_signature = j["thoughtSignature"].get<std::string>();
+        t.thought_signature = j.at("thought_signature").get<std::string>();
     }
 }
 
